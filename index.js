8aeb52764a2148a132a2dd5ae60a7f6916241ca8
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-29 14:57:39 -0300

    inicio aplicação express

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cartRoute = require('./src/routes/cartRoute.js')
+
+const app = express();
+const port = 3000;
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+   res.send('Hello world') 
+})
+
+app.get('/boas-vindas', (req, res) => {
+   res.send('Seja bem-vindo') 
+})
+
+app.use('/carrinho', cartRoute)
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na url http://localhost:${port}`)
+})
