626fe8a44509f8b4c9164c604d7426efee2fcb0e
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-29 17:39:15 -0300

    aula 1 express

--- db.js
+++ db.js
@@ -1,1 +1,25 @@
-//CREATE TABLE ...
+const {Client} = require('pg')
+
+
+const client = new Client ({
+    user:'postgres',
+    host:'localhost',
+    database: 'dc_store',
+    password: '1234',
+    port: 5432
+})
+
+
+//CREATE TABLE IF NOT EXISTS cart (
+//   id SERIAL PRIMARY KEY,
+//   user_id INTEGER,
+//   created_at TIMESTAMP DEFAULT CURRENT TIMESTAMP
+// );
+
+//CREATE TABLE IF NOT EXISTS cart_item (
+//   id SERIAL PRIMARY KEY,
+//   cart_id INTEGER REFERENCES cart(id) ON DELETE CASCADE,
+//   product_id INTEGER,
+//   quantity INTEGER DEFAULT 1
+//);
+
