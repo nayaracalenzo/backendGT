a8bed3000ea4c147bdd7d11598c0337fd602b667
nayaracalenzo <queiroz.14@hotmail.com>
2025-06-03 17:03:01 -0300

    inicio refatoração para prisma

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -83,11 +83,29 @@ router.put('/item/:id', async (req, res) => {
         details: error.message })
     }
  })
-router.delete('/item/:id', (req, res) => {
-    res.send('Deletar item carrinho') 
+router.delete('/item/:id', async (req, res) => {
+    const {id} = req.params
+    try {
+        const result = await client.query('DELETE FROM cart_item WHERE id = $1 RETURNING *',
+             [id])
+        
+        res.status(200).json({mensage: "Item deletado com sucesso", result: result.rows})     
+    } catch (error) {
+        console.log("Erro ao deletar item", error)
+        res.status(500).json({erro: "Erro ao deletar item",
+        details: error.message })
+    }
  })
-router.delete('/:id/itens', (req, res) => {
-    res.send('Deletar carrinho') 
+router.delete('/:id/itens', async (req, res) => {
+     const {id} = req.params
+     try {
+        await client.query('DELETE FROM cart_item WHERE cart_id = $1', [id])
+        res.status(200).json({mensage: "Carrinho limpo"})
+     } catch (error) {
+        console.log("Erro ao limpar carrinho", error)
+        res.status(500).json({erro: "Erro ao limpar carrinho",
+        details: error.message })
+     }
  })
 
 module.exports = router;
