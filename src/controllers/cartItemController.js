28364ac81d71de6926ee6ef7cbffc0778fc5b34e
nayaracalenzo <queiroz.14@hotmail.com>
2025-06-05 13:52:48 -0300

    mandei sem salvar

--- src/controllers/cartItemController.js
+++ src/controllers/cartItemController.js
@@ -2,18 +2,14 @@
 async function inserirItens() {
     const {cart_id, product_id, quantity} = req.body
     try {
-
-        const carrinhoCheck = await client.query('SELECT id FROM cart WHERE id = $1', [cart_id])
-        if (carrinhoCheck.rowCount === 0) {
-            return res.status(404).json({ error: 'Carrinho não encontrado'})
-        }
-
         const result = await inserirItensService(cart_id, product_id, quantity)
         res.status(201).json(result)
     } catch (error) {
         console.log("Erro ao inserir item no carrinho", error)
-        res.status(500).json({erro: "Erro ao adicionar item",
-        details: error.message })
+        res.status(error.status || 500).json(
+            {erro: error.message,
+            details: error.details || null
+        })
     }
    
 }
@@ -21,21 +17,15 @@ async function alterarItens() {
     const {id} = req.params
     const {quantity} = req.body
     try {
-        const result = await client.query(
-            'UPDATE cart_item SET quantity = $1 WHERE id = $2 RETURNING *',
-            [quantity, id]
-        )
-
-        if (result.rowCount === 0) {
-            return res.status(404).json({ error: 'Item não encontrado para alterar'})
-        }
-        res.status(200).json(result.rows[0])
+        const result = await alterarItensService(id, quantity)
+        res.status(200).json(result)
     } catch (error) {
         console.log("Erro ao alterar item", error)
-        res.status(500).json({erro: "Erro ao alterar item",
-        details: error.message })
+        res.status(error.status || 500).json(
+            {erro: error.message,
+            details: error.details || null
+        })
     }
-
 }
 async function deletarItens() {
     const {id} = req.params
--- src/services/cartItemService.js
+++ src/services/cartItemService.js
@@ -1,6 +1,9 @@
 
 async function inserirItensService(params) {
-    
+    const carrinhoCheck = await client.query('SELECT id FROM cart WHERE id = $1', [cart_id])
+        if (carrinhoCheck.rowCount === 0) {
+            return res.status(404).json({ error: 'Carrinho não encontrado'})
+        }
 
     if (!Number.isInteger(cart_id) || !Number.isInteger(product_id) || !Number.isInteger(quantity)) {
         return res.status(400).json({error: "Erro no corpo da requisição"})
@@ -16,6 +19,13 @@ async function alterarItensService(params) {
 
     if (!Number.isInteger(quantity) || quantity < 1) {
         return res.status(400).json({ error: "quantidade inválida. Deve ser número inteiro maior que 0"})
+    }
+    client.query(
+        'UPDATE cart_item SET quantity = $1 WHERE id = $2 RETURNING *',
+        [quantity, id]
+    )
+    if (result.rowCount === 0) {
+        return res.status(404).json({ error: 'Item não encontrado para alterar'})
     }
 }
 
