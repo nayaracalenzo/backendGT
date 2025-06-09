a9e3911dd349355850f4b0cc27d08ae3b1b60a21
nayaracalenzo <queiroz.14@hotmail.com>
2025-06-09 15:33:52 -0300

    intro middleware

--- src/routes/cartRoute.js
+++ src/routes/cartRoute.js
@@ -1,5 +1,8 @@
 const {  criarCarrinho, listarItens, limparCarrinho } = require('../controllers/cartController');
 const router = require('express').Router();
+const authMiddleware = require('../middleware/authMiddleware.js')
+
+router.use(authMiddleware)
 
 //listar carrinho itens
 router.get('/:id/itens',listarItens)
