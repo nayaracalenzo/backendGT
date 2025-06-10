f08e5a9ccabb5221150d51fe02a86354a25cb18f
nayaracalenzo <queiroz.14@hotmail.com>
2025-06-10 15:20:14 -0300

    todas as rotas funcionando

--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,9 +1,9 @@
 
-const { listarCarrinhoRepository, criarCarrinhoRepository, limparCarrinhoRepository } = require("../repositories/cartRepository");
+const { listarCarrinhoItensRepository, criarCarrinhoRepository, limparCarrinhoRepository } = require("../repositories/cartRepository");
 
 
 async function listarCarrinhoService(cart_id, user) {
-    return await listarCarrinhoRepository(cart_id, user)
+    return await listarCarrinhoItensRepository(cart_id, user)
 }
 async function criarCarrinhoService(user) {
     return await criarCarrinhoRepository(user)
